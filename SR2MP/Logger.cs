c962a5939a820962bb21ceb2f1d53b756839406b
py8 <74925512+pyeight@users.noreply.github.com>
2025-12-08 00:21:18 +0100

    Added sensitive logging

--- SR2MP/Logger.cs
+++ SR2MP/Logger.cs
@@ -8,6 +8,7 @@ namespace SR2MP
     {
         private static readonly MelonLogger.Instance melonLogger;
         internal static string logPath;
+        internal static string sensitiveLogPath;
 
         static Logger()
         {
@@ -18,14 +19,16 @@ namespace SR2MP
                 Directory.CreateDirectory(folderPath);
             
             logPath = Path.Combine(folderPath, "latest.log");
+            sensitiveLogPath = Path.Combine(folderPath, "sensitive.log");
             
             try
             {
                 File.WriteAllText(logPath, $"{DateTime.Now} [SR2MP] Log Initialized!");
+                File.WriteAllText(sensitiveLogPath, $"{DateTime.Now} [SR2MP] Sensitive Log Initialized!");
             }
             catch (Exception ex)
             {
-                melonLogger.Error($"Failed to initialize log file: {ex}");
+                melonLogger.Error($"Failed to initialize log files: {ex}");
             }
         }
 
@@ -35,34 +38,57 @@ namespace SR2MP
         {
             SR2ELogManager.SendMessage($"<size={sr2eSize}%>{message}</size>");
             melonLogger.Msg(message);
-            WriteToFile("[INFO]", message);
+            WriteToFile("[INFO]", message, false);
+        }
+
+        public static void LogSensitive(string message)
+        {
+            WriteToFile("[SENSITIVE]", message, true);
         }
 
         public static void Warn(string message)
         {
             SR2ELogManager.SendWarning(message);
             melonLogger.Warning(message);
-            WriteToFile("[WARNING]", message);
+            WriteToFile("[WARNING]", message, false);
         }
+        
+        public static void WarnSensitive(string message)
+        {
+            melonLogger.Warning("Warning created in sensitive.log!");
+            WriteToFile("[WARNING]", message, true);
+        }
+        
 
         public static void Error(string message)
         {
             SR2ELogManager.SendError(message);
             melonLogger.Error(message);
-            WriteToFile("[ERROR]", message);
+            WriteToFile("[ERROR]", message, false);
+        }
+        
+        public static void ErrorSensitive(string message)
+        {
+            melonLogger.Error("Error created in sensitive.log!");
+            WriteToFile("[ERROR]", message, true);
         }
 
         public static void Debug(string message)
         {
-            WriteToFile("[DEBUG]", message);
+            WriteToFile("[DEBUG]", message, false);
+        }
+        
+        public static void DebugSensitive(string message)
+        {
+            WriteToFile("[DEBUG]", message, true);
         }
 
-        private static void WriteToFile(string level, string message)
+        private static void WriteToFile(string level, string message, bool sensitive)
         {
             try
             {
                 string line = $"{DateTime.Now:HH:mm:ss} {level} {message}";
-                File.AppendAllText(logPath, "\n" + line);
+                File.AppendAllText(sensitive ? sensitiveLogPath : logPath, "\n" + line);
             }
             catch (Exception ex)
             {
