0e7ee000fc258db0ea8db5030df64c6fd975fa23
py8 <74925512+pyeight@users.noreply.github.com>
2025-12-08 21:05:41 +0100

    Initial Packet Utils

---
+++ SR2MP/Packets/PacketType.cs
@@ -0,0 +1,13 @@
+namespace SR2MP.Packets;
+
+public enum PacketType : byte
+{
+    Connect = 0,
+    ConnectAck = 1,
+    Close = 2,
+    PlayerJoin = 3,
+    PlayerLeave = 4,
+    PlayerUpdate = 5,
+    Heartbeat = 8,
+    HeartbeatAck = 9,
+}
