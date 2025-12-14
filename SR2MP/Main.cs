a2ea3a4c42ead60a46d9f45eceabed0f5d958793
shizophrenicgopher <aureumapes@duck.com>
2025-12-14 16:20:53 +0100

    Updated Assembly Metadata

--- SR2MP/Main.cs
+++ SR2MP/Main.cs
@@ -10,20 +10,19 @@ public sealed class Main : SR2EExpansionV2
         public const string Name = "Slime Rancher 2 Multiplayer Mod";
         public const string Description = "Adds Multiplayer to Slime Rancher 2";
         public const string Author = "Shark";
-        public const string CoAuthors = null;
+        public const string CoAuthors = "";
         public const string Contributors = "Gopher, Artur, AlchlcSystm";
-        public const string Company = null;
-        public const string Version = "0.1.0";
-        public const string DownloadLink = null;
+        public const string Company = "";
+        public const string Version = "0.1.0.1";
+        public const string DownloadLink = "";
         public const string SourceCode = "https://github.com/pyeight/SlimeRancher2Multiplayer";
-        public const string Nexus = null;
+        public const string Nexus = "";
         public const string Discord = "https://discord.com/invite/a7wfBw5feU";
         public const bool UsePrism = false;
     }
 
     public override void OnSceneWasLoaded(int _, string sceneName)
     {
-        
     }
 
     public override void OnLateInitializeMelon()
