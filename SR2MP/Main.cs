ce7b63f8a168cc374f852d5a18f779e3d3f03384
shizophrenicgopher <aureumapes@duck.com>
2025-12-20 15:24:49 +0100

    Added Username Preference

--- SR2MP/Main.cs
+++ SR2MP/Main.cs
@@ -1,4 +1,6 @@
-﻿using SR2E.Expansion;
+﻿using MelonLoader;
+using MelonLoader.Utils;
+using SR2E.Expansion;
 using SR2MP.Shared.Utils;
 
 namespace SR2MP;
@@ -7,8 +9,14 @@ public sealed class Main : SR2EExpansionV3
 {
     public static Client.Client Client { get; private set; }
     public static Server.Server Server { get; private set; }
+    static MelonPreferences_Category prefs;
+    public static string username => prefs.GetEntry<string>("username").Value;
+
     public override void OnLateInitializeMelon()
     {
+        prefs = MelonPreferences.CreateCategory("SR2MP");
+        prefs.CreateEntry("username", "Player");
+
         Client = new Client.Client();
         Server = new Server.Server();
     }
--- SR2MP/Packets/C2S/PlayerJoinPacket.cs
+++ SR2MP/Packets/C2S/PlayerJoinPacket.cs
@@ -8,16 +8,19 @@ public struct PlayerJoinPacket : IPacket
 {
     private byte Type { get; set; }
     private string PlayerId { get; set; }
+    private string PlayerName { get; set; }
 
     public readonly void Serialise(PacketWriter writer)
     {
         writer.WriteByte(Type);
         writer.WriteString(PlayerId);
+        writer.WriteString(PlayerName);
     }
 
     public void Deserialise(PacketReader reader)
     {
         Type = reader.ReadByte();
         PlayerId = reader.ReadString();
+        PlayerName = reader.ReadString();
     }
 }
